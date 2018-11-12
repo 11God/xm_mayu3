6b18ffefb998b156fbeeff8ffcdf1cbb0818e01a
11God <17703283065@163.com>
2018-11-12 15:23:21 +0800

    first commit

---
+++ scripts/test_contact.py
@@ -0,0 +1,22 @@
+
+class TestContact:
+
+    def test_add1(self):
+        print('hello1')
+        assert 1
+
+    def test_add2(self):
+        print('hello2')
+        assert 1
+
+    def test_add3(self):
+        print('hello3')
+        assert 1
+
+    def test_add4(self):
+        print('hello4')
+        assert 0
+
+
+
+
